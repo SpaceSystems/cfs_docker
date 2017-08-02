ea4a205098a7ee1c209f74d1a3b64796123a2072
Randy Ransier <Randy.Ransier@icloud.com>
2017-08-02 18:10:28 -0400

    Exposed docker port 1234 for CFS to receive commands.

--- run_container.sh
+++ run_container.sh
@@ -2,5 +2,6 @@
 CONTAINER="cfs_docker"
 
 sudo docker run --rm -ti -v $PWD/shared_env:/home/shared --privileged \
-                --sysctl fs.mqueue.msg_max=2000 $CONTAINER /bin/bash
+                --sysctl fs.mqueue.msg_max=2000 -p 127.0.0.1:1234:1234/udp \
+                $CONTAINER /bin/bash
 
